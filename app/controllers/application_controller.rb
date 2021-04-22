dd3e8e948c92f5843c974a5b0df792b3233cfe27
acparkgh <acpark123@gmail.com>
2021-04-22 17:00:05 -0400

    created routes controller views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+
+  def home
+    puts "We have hit our home action"
+    @cohort_name = "Hello World!"
+    render "home"
+  end
+
+  def hello
+    @cohort_name = "Ruby Dooby Doo"
+    render "hello"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "/home", to: "application#home"
+  get "/", to: "application#home"
+  get "/hello", to: "application#hello"
+
 end
